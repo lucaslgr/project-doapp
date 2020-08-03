1a90dcbef176fe373c20d4fc21b0330e94a7323a
Lucas Guimaraes <lucaslgr1206@gmail.com>
2020-08-03 15:03:41 -0300

    Mudando as configuracoes no workbox-config.jg para setar os caches para imagens específicas em fodlers específicos e setando os arquivos que devem ser ignorados para o pre-caching

--- frontend/workbox-config.js
+++ frontend/workbox-config.js
@@ -1,7 +1,13 @@
 module.exports = {
   "globDirectory": "public/",
   "globPatterns": [
-    "**/*.{html,json,css,eot,svg,ttf,woff,woff2,png,js}"
+    "**/*.{html,json,css,eot,svg,ttf,woff,woff2,js}",
+    // "src/images/**/*.{jpg,png}",
+    "src/images/logos/*.{jpg,png}",
+    "src/images/products/*.{jpg,png}",
   ],
-  "swDest": "public/sw-workbox.js"
+  "swDest": "public/sw-workbox.js",
+  "globIgnores": [
+    "help.html"
+  ]
 };
