e3709af4533e8c3132f352d0d2be2c5dca8b543f
Lucas Guimaraes <lucaslgr1206@gmail.com>
2020-08-24 13:12:02 -0300

    Sincronizacao de arquivos

--- frontend/public/src/js/dist/checkIsLoggedUser.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-//Verifica se o usuario esta logado, se não estiver, redireciona para o index.html
-//Checando se o JWT e o id do usuário logado estão setados
-if (window.localStorage.getItem('jwt') == '' || window.localStorage.getItem('jwt') == 'null' || window.localStorage.getItem('jwt') == 'undefined' || window.localStorage.getItem('jwt') == undefined || window.localStorage.getItem('id_logged_user') == '' || window.localStorage.getItem('id_logged_user') == 'undefined' || window.localStorage.getItem('id_logged_user') == undefined || window.localStorage.getItem('id_logged_user') == 'null') {
-  window.location.href = BASE_URL;
-}
